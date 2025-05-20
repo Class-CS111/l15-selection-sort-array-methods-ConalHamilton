da4d42f5e1ac45e3f98630e998751cf9a6b466bc
Conal <122840957+ConalHamilton@users.noreply.github.com>
2025-05-20 03:42:10 -0700

    Update ArrayMethods.java

--- ArrayMethods.java
+++ ArrayMethods.java
@@ -1,8 +1,8 @@
 /********************************************
-*	AUTHOR:	<name>
-* COLLABORATORS: <names>
+*	AUTHOR:	Conal Hamilton
+* COLLABORATORS: None
 *	COURSE:	CS 111 Intro to CS I - Java
-*	LAST MODIFIED: <date>
+*	LAST MODIFIED: May 20 2025
 ********************************************/
 
 /********************************************
@@ -10,9 +10,6 @@
 *********************************************
 *	PROGRAM DESCRIPTION:
 *	Collection of useful methods for int arrays.
-*********************************************
-*	ALGORITHM:
-*	TODO: <Pseudocode for selection sort here>
 *********************************************
 
 /* UML CLASS DIAGRAM:
@@ -30,34 +27,88 @@ ArrayMethods
 
 public class ArrayMethods
 {
-  /**DESCRIPTION: */
+  /**
+   * Assembles and returns string containing contents of an int array
+   * 
+   * @param a int array of values, must be of 1 length or greater
+   * 
+   * @return contents of array in String separated by commas and concatenated with brackets
+   * 
+  */
   public static String arrayString(int[] a)
   {
-    return ""; //STUB to keep compiler happy
+    String results = "{ " + a[0];
+    for (int i = 1; i < a.length; i++)
+    {
+      results += ", " + a[i];
+    }
+    results += " }";
+    return results;
   }
   
-  /**DESCRIPTION: */
+  /**
+   * Swaps two values of an int array at the given locations
+   * 
+   * @param array int array to be modified
+   * @param a array index of first location to swap, must be valid index number
+   * @param b array index of second location to swap, must be valid index number
+  */
   public static void swap(int[] array, int a, int b)
   {
-  
+    int valFromA = array[a];
+    int valFromB = array[b];
+    array[a] = valFromB;
+    array[b] = valFromA;
   }
 	
-	/**DESCRIPTION: */
+	/**
+   * Identifies and returns the index number of the smallest value in a given int array, ignoring all values before the given index
+   * 
+   * @param array int array to be analyzed
+   * @param startIndex index of value to start analysis at, earlier values in the array will be ignored, must be valid array index
+   * @return index of smallest value in the array, ignoring all values before that held at startIndex
+  */
   public static int indexOfMin(int[] array, int startIndex)
   {
-    return 0; //STUB to keep compiler happy
+    int testValue = array[startIndex];
+    int testValueIndex = startIndex;
+    for (int i = startIndex; i < array.length; i++)
+    {
+      if(testValue >= array[i])
+      {
+        testValue = array[i];
+        testValueIndex = i;
+      }
+    }
+    return testValueIndex;
   }
 
-	/**DESCRIPTION: */
+	/**
+   * Reverses the order of a given int array
+   * 
+   * @param array int array to be reversed, will be modified
+  */
   public static void reverse(int[] array)
   {
-
+    int halfLength = array.length/2;
+    for(int i = 0; i < halfLength; i++)
+    {
+      ArrayMethods.swap(array, i, array.length-i-1);
+    }
   }
 
-	/**DESCRIPTION: */
+	/**
+   * Sorts a given int array by ascending value
+   * 
+   * @param array int array to be sorted, will be modified
+  */
   public static void selectionSort(int[] array)
   {
-
+    for (int i = 0; i < array.length; i++) 
+    {
+      int indexMinValue = ArrayMethods.indexOfMin(array, i);
+      ArrayMethods.swap(array, i, indexMinValue);
+    }
   }
 
 }
